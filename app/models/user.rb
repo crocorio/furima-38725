243099f43efdcb4f0553aca381fee967c9a79176
crocorio <miyakonojououdo@gmail.com>
2022-12-22 12:43:57 +0900

    validation 正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true 
-         validates :last_name, presence: true 
-         validates :first_name, presence: true 
-         validates :last_name_kana, presence: true 
-         validates :first_name_kana, presence: true 
-         validates :birthday, presence: true 
+         with_options presence: true do 
+         validates :nickname
+         validates :birthday
+         end 
 
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' 
+
+         
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+          validates :first_name
+          validates :last_name
+        end
+        
+
+        
+        with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+         validates :last_name_kana
+         validates :first_name_kana
+        end 
+         
          
          
 end
