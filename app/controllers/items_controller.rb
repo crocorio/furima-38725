d6622f1ecd7ca4a475e8cf4fa7fa80c6fa90c5f1
crocorio <miyakonojououdo@gmail.com>
2022-12-26 19:52:05 +0900

    created route forもどる, made it so that edit and update cant be accessed directly

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('id DESC')
@@ -27,6 +28,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end 
 
   private
@@ -35,4 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :description, :category_id,
                                  :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end 
+
 end
