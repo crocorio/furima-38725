68f1f914dbab770cba0ef340939feb621b0a9af3
crocorio <miyakonojououdo@gmail.com>
2022-12-26 20:13:21 +0900

    deleted comment out, performed rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit 
+  def edit
     @item = Item.find(params[:id])
   end
 
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-  end 
+  end
 
   private
 
@@ -45,9 +45,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
-  end 
-
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+  end
 end
