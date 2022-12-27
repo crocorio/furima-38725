997070d5d7bd5699ca40740c17f4085be77a32b8
crocorio <miyakonojououdo@gmail.com>
2022-12-27 20:19:50 +0900

    moved validations from destination and order

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,17 +1,12 @@
 class Destination < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
+  
 
 
-  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/ 
-  validates :postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
+  
 
 
 
-  validates :city,        presence: true
-  validates :block,       presence: true
-  validates :building
-  validates :phone_number,    presence: true, format: {with: /\A\d{11}\z/}
+  
 
   belongs_to :order 
 
---
+++ app/models/order_destination.rb
@@ -0,0 +1,26 @@
+class OrderDestination 
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :order_id
+
+
+
+  with_options presence: true do
+  validates :user_id
+  validates :item_id
+
+  validates :city
+  validates :block
+  
+  validates :phone_number, format: {with: /\A\d{11}\z/}
+
+  
+  validates :postal_code,  format: { with: /\A\d{3}[-]?\d{4}\z/  }
+
+  
+  end 
+
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  validates :building
+
+end
