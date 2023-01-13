40912b78763742a095d073f4b07f91ba60aed217
crocorio <miyakonojououdo@gmail.com>
2023-01-13 18:01:13 +0900

    changed the position of the line just in case

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '43.207.12.118', user: 'ec2-user', roles: %w{app db web}
+
 
 
 # role-based syntax
@@ -59,3 +59,5 @@ server '43.207.12.118', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+server '43.207.12.118', user: 'ec2-user', roles: %w{app db web}
